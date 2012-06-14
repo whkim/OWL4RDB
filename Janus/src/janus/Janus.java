3213d4c0f1df25bd46f1c7da05fd41f4fd55c86c
whkim <kwh708@gmail.com>
2012-06-14 19:58:30 +0900

    commit

--- Janus/src/janus/Janus.java
+++ Janus/src/janus/Janus.java
@@ -14,6 +14,8 @@ public class Janus {
 		
 		setLookAndFeel();
 		
+		System.out.println("hello");
+		
 		JanusWindow mainWindow = new JanusWindow();
 		splash.dispose();
 		mainWindow.setVisible(true);
